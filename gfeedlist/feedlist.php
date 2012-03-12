d7bcf52d143fa636437c69c25a309fe4cdc9c0e1
taoanton <taoanton@gmail.com>
2012-03-12 18:04:26 +0800

    change feedlist

--- gfeedlist/feedlist.php
+++ gfeedlist/feedlist.php
@@ -1,107 +1,10 @@
 <?php 
 set_time_limit(0);
-require_once "app/Mage.php";
-/*require_once "feedlist_item.php";*/
+require_once "../app/Mage.php";
+require_once "feedlist_item.php";
 Mage::app('default');
 define('SEP',"\t");
 
-$google_list_item['id'] = array(
-	'title' => 'id',
-	'method' => 'getid',
-	'value' => '',
-);
-$google_list_item['name'] = array(
-	'title' => 'title',
-	'method' => 'getName',
-	'value' => '',
-);
-$google_list_item['description'] = array(
-	'title' => 'description',
-	'method' => 'getDescription',
-	'value' => '',
-);
-$google_list_item['link'] = array(
-	'title' => 'link',
-	'method' => 'getProductUrl',
-	'value' => '',
-);
-$google_list_item['image_link'] = array(
-	'title' => 'image link',
-	'method' => 'getImageUrl',
-	'value' => '',
-);	
-$google_list_item['price'] = array(
-	'title' => 'price',
-	'method' => 'getPrice',
-	'value' => 'pr',
-);                                                      
-$google_list_item['sale_price'] = array(
-	'title' => 'sale price',
-	'method' => 'getPrice',
-	'value' => 'pr',
-);
-/*$google_list_item['sale_price_date'] = array(
-	'title' => 'sale price effective date',
-	'method' => 'getSpecialFromDate',
-	'value' => '',
-);*/
-$google_list_item['shipping_weight'] = array(
-	'title' => 'shipping weight',
-	'method' => 'getWeight',
-	'value' => 'fl',
-);
-$google_list_item['product_type'] = array(
-	'title' => 'product type',
-	'method' => '',
-	'value' => 'type',
-);
-$google_list_item['item_group_id'] = array(
-	'title' => 'item group id',
-	'method' => 'getSku',
-	'value' => '',
-);
-$google_list_item['mpn'] = array(
-	'title' => 'mpn',
-	'method' => 'getSku',
-	'value' => '',
-);
-
-$google_list_item_attr['color'] = array(
-	'title' => 'color',
-	'value' => '',
-);
-$google_list_item_attr['brand'] = array(
-	'title' => 'brand',
-	'value' => '',
-);
-$google_list_item_attr['age_group'] = array(
-	'title' => 'age group',
-	'value' => '',
-);		
-$google_list_item_attr['gender'] = array(
-	'title' => 'gender',
-	'value' => '',
-);	
-$google_list_item_attr['gtin'] = array(
-	'title' => 'gtin',
-	'value' => '',
-);	
-/*$google_list_item_attr['size'] = array(
-	'title' => 'size',
-	'value' => '',
-);	*/	
-
-
-$google_list_item_fixinfo['tax'] = array(
-	'title' => 'tax',
-);
-$google_list_item_fixinfo['shipping'] = array(
-	'title' => 'shipping',
-);$google_list_item_fixinfo['excluded_destination'] = array(
-	'title' => 'excluded destination',
-);$google_list_item_fixinfo['expiration_date'] = array(
-	'title' => 'expiration date',
-);
 
 
 /* currency */
@@ -114,6 +17,7 @@ foreach($res as $re){
 	$currency_rate[$re['currency_to']] = $re['rate'];
 }
 $basecurrency = $re['currency_from'];
+
 
 function nodeToArray(Varien_Data_Tree_Node $node)
 {
